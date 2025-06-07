613e8d500221def547e8b60bede14831839d2a1a
Priom Halder <priomhalder1470@gmail.com>
2025-06-08 01:28:29 +0600

    Add files via upload

---
+++ making a pyramid.py
@@ -0,0 +1,35 @@
+# making a pyramid
+
+'''n=5
+i=0
+while(i<n):
+    j=0
+    while (j<=n-1-i):
+        print(" ",end="")
+        j=j+1
+    k=0
+    while (k<=i):
+        print("* ",end="")
+        k=k+1
+    print("")
+    i=i+1
+
+# making a reversed pyramid
+
+# reversed pyramid
+n=5
+i=0
+while(i<n):
+    j=0
+    while (j<=i):
+        if j==0:
+            pass
+        else:
+          print(" ",end="")
+        j=j+1
+    k=0
+    while (k<=n-1-i):
+        print("* ",end="")
+        k=k+1
+    print("")
+    i=i+1'''
