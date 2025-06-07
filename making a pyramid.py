4ee53832580c23fd1c4029cccb535bb5ffd3460a
Priom Halder <priomhalder1470@gmail.com>
2025-06-08 01:29:27 +0600

    Making a pyramid.py

--- making a pyramid.py
+++ making a pyramid.py
@@ -14,22 +14,3 @@ while(i<n):
     print("")
     i=i+1
 
-# making a reversed pyramid
-
-# reversed pyramid
-n=5
-i=0
-while(i<n):
-    j=0
-    while (j<=i):
-        if j==0:
-            pass
-        else:
-          print(" ",end="")
-        j=j+1
-    k=0
-    while (k<=n-1-i):
-        print("* ",end="")
-        k=k+1
-    print("")
-    i=i+1
